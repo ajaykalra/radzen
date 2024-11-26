6866d6a1ed0e63c39c7a422ed4740a94be6f5c4f
Ajay Kalra <ajkalra@gmail.com>
2024-11-26 17:41:14 -0500

    sample page instatiated from sidebar

--- Radzen.Samples.Blazor.Sudio/Components/Pages/Pager.razor.cs
+++ Radzen.Samples.Blazor.Sudio/Components/Pages/Pager.razor.cs
@@ -1,18 +1,16 @@
-using System.Net.Http;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Radzen;
 using Radzen.Blazor;
 
-namespace Blazor.Server.Sample.Components.Layout
+namespace Blazor.Server.Sample.Components.Pages
 {
-    public partial class MainLayout
+    public partial class Pager
     {
         [Inject]
         protected IJSRuntime JSRuntime { get; set; }
@@ -31,17 +29,5 @@ namespace Blazor.Server.Sample.Components.Layout
 
         [Inject]
         protected NotificationService NotificationService { get; set; }
-
-        private bool sidebarExpanded = true;
-
-        void SidebarToggleClick()
-        {
-            sidebarExpanded = !sidebarExpanded;
-        }
-
-        protected void onclick(MouseEventArgs item)
-        {
-            
-        }
     }
 }
